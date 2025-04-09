b58b8fa2bd1d8c6bd79e4628066b3037538c5618
leeminjin0827 <lmj98202@gmail.com>
2025-04-09 12:33:29 +0900

    04/07 Todo java

---
+++ src/main/java/example/day04/AppStart.java
@@ -0,0 +1,13 @@
+package example.day04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing // JPA 감사 기능 활성화 = 레코드(영속) 등록/수정 시점
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    } // m end
+} // c end

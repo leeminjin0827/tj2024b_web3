cec5a130a1e062c519c37c4a16f71fea94c5df92
leeminjin0827 <lmj98202@gmail.com>
2025-04-17 14:58:56 +0900

    04/17 토큰 활용한 중복 로그인 방지 , 로그아웃

--- src/main/java/web/controller/MemberController.java
+++ src/main/java/web/controller/MemberController.java
@@ -2,8 +2,10 @@ package web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web.model.dto.MemberDto;
@@ -19,12 +21,43 @@ public class MemberController {
     // -> 즉 final 사용시 @RequiredArgsConstructor 때문에 @Autowired 안해도 된다.
     private final MemberService memberService;
 
-    // 가입
-    @PostMapping("/post")
+    // 가입 // { "memail" : "qwe@naver.com" , "mpwd" : "qwe" , "mname" : "유재석" }
+    @PostMapping("/signup") // http://localhost:8080/member/signup
     public boolean MemberWrite(@RequestBody MemberDto memberDto){
         System.out.println("MemberController.MemberWrite");
         System.out.println("memberDto = " + memberDto);
         return memberService.MemberWrite( memberDto );
+    } // f end
+
+    // 로그인 // { "memail" : "qwe@naver.com" , "mpwd" : "qwe" }
+    @PostMapping("/login") // http://localhost:8080/member/login
+    public String login( @RequestBody MemberDto memberDto ){
+        return memberService.login( memberDto );
+    } // f end
+
+    // 로그인된 회원 검증 / 내정보 조회
+    // @RequestHeader : HTTP 헤더 정보를 매핑 하는 어노테이션 , JWT 정보는 HTTP 헤더 에 담을 수 있다.
+    // Authorization : 인증 속성 , { Authorization : token 값 }
+    // @RequestParam : HTTP 헤더의 경로 쿼리스트링 매핑 하는 어노테이션
+    // @RequestBody : HTTP 본문의 객체를 매핑 하는 어노테이션
+    // @PathVariable : HTTP 헤더의 경로 값 매핑하는 어노테이션
+    @GetMapping("/info") // http://localhost:8080/member/info
+    // headers : { 'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJxd2VAbmF2ZXIuY29tIiwiaWF0IjoxNzQ0NzcxNTM0LCJleHAiOjE3NDQ4NTc5MzR9.g8sM_lX31AgbILTQMJXGEzX5K2F6Z6ak-mBweZmpM-I'}
+    public MemberDto info(@RequestHeader("Authorization") String token ){
+        System.out.println( token );
+        return memberService.info( token );
+    } // f end
+
+    // 로그아웃 , 로그아웃 할 토큰 가져오기.
+    @GetMapping("/logout") // http://localhost:8080/member/logout
+    public void logout( @RequestHeader("Authorization") String token ){
+        memberService.logout( token );
+    } // f end
+
+    // 최근 24시간내 로그인 한 접속자 수
+    @GetMapping("/login/count")
+    public int loginCount(){
+        return memberService.loginCount();
     } // f end
 
 } // c end
--- src/main/java/web/service/MemberService.java
+++ src/main/java/web/service/MemberService.java
@@ -2,11 +2,17 @@ package web.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import web.model.dto.MemberDto;
 import web.model.entity.MemberEntity;
 import web.model.repository.MemberRepository;
+import web.util.JwtUtil;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Service // Spring MVC2 service
 @RequiredArgsConstructor // final 필드의 생성자 자동 생성
@@ -33,5 +39,66 @@ public class MemberService {
         // 4. 영속된 엔티티의 (자동생성된) pk 확인
         if( saveEntity.getMno() >= 1 ){ return true; }
         return false;
+    } // f end
+
+    // * JWT 객체 주입
+    private final JwtUtil jwtUtil;
+
+    // [2] 로그인 , 로그인 성공시 token 실패시 null
+    public String login( MemberDto memberDto ){
+        // 1. 이메일(아이디)를 DB에서 조회하여 엔티티 찾기
+        MemberEntity memberEntity
+                = memberRepository.findByMemail( memberDto.getMemail() );
+        // 2. 조회된 엔티티가 없으면
+        if( memberEntity == null ){ return null; } // 로그인 실패
+        // 3. 조회된 엔티티의 비밀번호 검증. .matches( 입력받은패스워드 , 암호화된패스워드 )
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // Bcrypt 객체 생성
+        boolean inMath
+                = passwordEncoder.matches( memberDto.getMpwd() , memberEntity.getMpwd() );
+        // 4. 비밀번호 검증 실패하면
+        if( inMath == false ){ return null; } // 로그인 실패
+        // 5. 비밀번호 검증 성공이면 Token 발급 vs 세션 부여/발급
+        String token
+                = jwtUtil.createToken( memberEntity.getMemail() );
+        System.out.println(" >> 발급된 token : " + token );
+
+        // + 레디스에 24시간만 저장되는 로그인 로그(기록) 하기.
+        stringRedisTemplate.opsForValue().set(
+                "RECENT_LOGIN:"+memberEntity.getMemail() , "true" , 1 , TimeUnit.DAYS );
+
+        return token;
+    } // f end
+
+    // [3] 전달받은 token 으로 token 검증하여 유효한 token 은 회워정보(dto) 반환 유효하지 않은 token null 반환
+    public MemberDto info( String token ){
+        // 1. 전달받은 token 으로 검증하기. vs 세션 호출/검증
+        String memail = jwtUtil.validateToken( token );
+        // 2. 검증이 실패이면 유효기간 만료 , 실패
+        if( memail == null ){ return null; }
+        // 3. 검증이 성공이면 토큰에 저장된 이메일을 가지고 엔티티 조회
+        MemberEntity memberEntity
+                = memberRepository.findByMemail( memail );
+        // 4. 조회된 엔티티가 없으면 실패
+        if( memberEntity == null ) { return null; }
+        // 5. 조회 성공시 조회된 엔티티를 dto로 변환하여 반환
+        return memberEntity.toDto();
+    } // f end
+
+    // [4] 로그아웃 시 redis에 저장된 토큰 삭제 서비스
+    public void logout( String token ){
+        // 1. 해당 token의 이메일 조회
+        String memail = jwtUtil.validateToken( token );
+        // 2. 조회된 이메일의 redis 토큰 삭제
+        jwtUtil.deleteToken( memail );
+    } // f end
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    // 최근 24시간내 로그인 한 접속자 수
+    public int loginCount(){ // * 로그인시 레디스에 "RECENT_LOGIN" 이름으로 key 저장
+        // 레디스에 저장된 키 들 중에서 "RENECT_LOGIN:" 으로 시작하는 모든 KEY 반환
+        Set<String> keys = stringRedisTemplate.keys("RECENT_LOGIN:*");
+        // 반환된 개수 확인 , 비어있으면 0 아니면 size() 함수 이용한 key 개수 반환
+        return keys == null ? 0 : keys.size();
     } // f end
 } // c end
